0900f64981143453812d943380f4518d7d56030a
philoniare <philoniare@gmail.com>
2016-06-22 21:12:18 -0400

    Refactored to use fragments instead of activities

--- app/src/main/java/com/example/philoniare/tourguideapp/TourGuide.java
+++ app/src/main/java/com/example/philoniare/tourguideapp/TourGuide.java
@@ -1,5 +1,9 @@
 package com.example.philoniare.tourguideapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -7,7 +11,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class TourGuide extends AppCompatActivity
@@ -28,8 +31,39 @@ public class TourGuide extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Start Attractions fragment
+        startFragment("attractionsFrag");
     }
 
+    public void startFragment(String fragTag) {
+        FragmentManager fManager = getFragmentManager();
+        FragmentTransaction fTransaction = fManager.beginTransaction();
+        Fragment fragment = fManager.findFragmentByTag(fragTag);
+
+        // If fragment doesn't exist yet, create one
+        if (fragment == null) {
+            switch(fragTag) {
+                case "attractionsFrag":
+                    fTransaction.add(R.id.main_fragment_container, new AttractionsFragment(), fragTag);
+                    break;
+                case "parksFrag":
+                    fTransaction.add(R.id.main_fragment_container, new ParksFragment(), fragTag);
+                    break;
+                case "startupsFrag":
+                    fTransaction.add(R.id.main_fragment_container, new StartupsFragment(), fragTag);
+                    break;
+                case "restaurantsFrag":
+                    fTransaction.add(R.id.main_fragment_container, new RestaurantsFragment(), fragTag);
+                    break;
+            }
+
+
+        }
+        else { // re-use the old fragment
+            fTransaction.replace(R.id.main_fragment_container, fragment, fragTag);
+        }
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -40,47 +74,22 @@ public class TourGuide extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.tour_guide, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        Intent navIntent;
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_attractions) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_startups) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_parks) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else {
 
         }
+        // check if it is the current activity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
